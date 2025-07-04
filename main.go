de889f05442313b094508afa74a31ad60d6056ca
miraclemenikelechi <miracle.emenike7@gmail.com>
2025-07-04 05:47:57 +0100

    add data type examples to main.go

--- main.go
+++ main.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("hello golang")
+	fmt.Println("### Data Types in Golang ###")
+	fmt.Println("This is a string data type => ", "Hello World")
+	fmt.Println("This is a boolean data type => ", true)
+	fmt.Println("This is a float data type => ", 3.14)
+	fmt.Println("This is a integer data type => ", 100)
+	fmt.Println("This is a complex data type => ", 2i)
+	fmt.Println()
+	fmt.Println("but as you see, they are all printed in strings. now let's see their types properly.")
+	fmt.Println()
+	fmt.Println("This is a string data type => ", fmt.Sprintf("%T", "Hello World"))
+	fmt.Println("This is a boolean data type => ", fmt.Sprintf("%T", true))
+	fmt.Println("This is a float data type => ", fmt.Sprintf("%T", 3.14))
+	fmt.Println("This is a integer data type => ", fmt.Sprintf("%T", 100))
+	fmt.Println("This is a complex data type => ", fmt.Sprintf("%T", 2i))
 }
