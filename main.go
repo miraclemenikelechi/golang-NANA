f45876eb5f368624e7b9353989d7d8b273f72114
miraclemenikelechi <miracle.emenike7@gmail.com>
2025-07-04 10:41:14 +0100

    update main.go to correct output message and streamline fruit loop implementation

--- main.go
+++ main.go
@@ -3,16 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("### Arrays and Slices in Golang ###")
+	fmt.Println("### Loops in Golang ###")
 
-	var arrayOfNumbers = []int{1, 2, 3, 4, 5}
-	fmt.Println(arrayOfNumbers)
+	fruits := []string{"apple", "banana", "mango", "orange", "watermelon"}
+	fmt.Println("Fruits:", fruits)
 
-	arrayOfStrings := [2]string{"a", "b", "c", "d", "e"}
-	fmt.Println(arrayOfStrings)
+	for index, value := range fruits {
+		fmt.Printf("Index: %d, Value: %s\n", index, value)
+	}
 
-	// array is fixed ie `arrayOfStrings` is an actual array in golang
-	// slices are dynamic ie `arrayOfNumbers` is a slice
-
-	// the code will not execute until `arrayOfStrings` holds the actual amount of data that has been assigned to it. currently it is more than.
 }
