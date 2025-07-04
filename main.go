a4e7f042df1122de6b5f6c19c65a6a1d3f96fbbd
miraclemenikelechi <miracle.emenike7@gmail.com>
2025-07-04 06:37:56 +0100

    fix: correct array declaration for arrayOfStrings and update comments for clarity

--- main.go
+++ main.go
@@ -8,7 +8,11 @@ func main() {
 	var arrayOfNumbers = []int{1, 2, 3, 4, 5}
 	fmt.Println(arrayOfNumbers)
 
-	arrayOfStrings := []string{"a", "b", "c", "d", "e"}
+	arrayOfStrings := [2]string{"a", "b", "c", "d", "e"}
 	fmt.Println(arrayOfStrings)
 
+	// array is fixed ie `arrayOfStrings` is an actual array in golang
+	// slices are dynamic ie `arrayOfNumbers` is a slice
+
+	// the code will not execute until `arrayOfStrings` holds the actual amount of data that has been assigned to it. currently it is more than.
 }
